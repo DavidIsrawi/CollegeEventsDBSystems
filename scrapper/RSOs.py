f69988394b1c289dd1da38a96fc6338ca91117d4
David <davidisrawi@gmail.com>
2017-03-23 21:54:08 -0400

    all clubs in json + scrapper

--- scrapper/RSOs.py
+++ scrapper/RSOs.py
@@ -6,37 +6,48 @@ import json
 rsoLst = list()
 
 class RSO:
-    def __init__(self, name, description, image):
+    def __init__(self, name, description, image, owner):
         self.name = name
         self.description = description
         self.image = image
+        self.owner = owner
 
+for i in range(1,67):
+    # Request page
+    fPage = "https://ucf.collegiatelink.net/organizations?SearchType=None&SelectedCategoryId=0&CurrentPage=" + str(i)
+    result = requests.get(fPage)
 
-# Request page
-fPage = "https://ucf.collegiatelink.net/organizations"
-result = requests.get(fPage)
+    # Get content from website
+    content = result.content
 
-# Get content from website
-content = result.content
+    # Beautify
+    soup = BeautifulSoup(content)
 
-# Beautify
-soup = BeautifulSoup(content)
+    rsos = soup.find_all("div", "result clearfix")
+    for rso in rsos:
 
-rsos = soup.find_all("div", "result clearfix")
-for rso in rsos:
-    name = rso.find("a").next
-    description = rso.find("p").next
+        profile = rso.find("h5").find("a").get("href")
+        # Request page
+        profilePage = "https://ucf.collegiatelink.net" + profile
+        profileResult = requests.get(profilePage)
+        profileContent = profileResult.content
+        soupP = BeautifulSoup(profileContent)
+        owner = soupP.find("div", "container-orgcontact").findAll("li")[1].text[18:].strip()
 
-    if rso.find("img") is not None:
-        picture = rso.find("img").get('src')
-    else:
-        picture = ""
+        name = rso.find("a").next
+        description = rso.find("p").next
 
-    print("\n\n\nClub Information:")
-    print(name)
-    print(description)
-    print(picture)
-    rsoLst.append(RSO(name,description,picture))
+        if rso.find("img") is not None:
+            picture = rso.find("img").get('src')
+        else:
+            picture = ""
+
+        print("\n\n\nClub Information:")
+        print(name)
+        print(description)
+        print(picture)
+        print(owner)
+        rsoLst.append(RSO(name,description,picture,owner))
 
 with open('RSOs.json', 'w') as outfile:
     json.dump(rsoLst, outfile, default=lambda o: o.__dict__, indent=4)
