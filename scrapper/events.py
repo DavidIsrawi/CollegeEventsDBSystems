7305b96a398107eadb7067bc68f5a6052ba0dcbe
David <davidisrawi@gmail.com>
2017-03-25 16:52:50 -0400

    All events from 03/01-05/26 + scrapper

--- scrapper/events.py
+++ scrapper/events.py
@@ -4,6 +4,8 @@ import feedparser
 import json
 import re
 
+# Implement feeds per day (http://events.ucf.edu/2017/3/25/feed.xml)
+
 eventL = list()
 
 class eventC:
@@ -17,38 +19,40 @@ class eventC:
         self.email = email
         self.tags = tags
 
-page = "http://events.ucf.edu/feed.xml"
-result = requests.get(page)
-# Get content from website
-content = result.content
-# Beautify
-soup = BeautifulSoup(content, 'xml')
+for month in range (3,6):
+    for day in range(1,31):
+        page = "http://events.ucf.edu/2017/" + str(month) + "/" + str(day) + "/feed.xml"
+        result = requests.get(page)
+        # Get content from website
+        content = result.content
+        # Beautify
+        soup = BeautifulSoup(content, 'xml')
 
-events = soup.findAll('event')
-for event in events:
-    print("\n\n\n\n Event:")
-    title = event.find('title').next
-    eid = event.find('event_id').next
-    location = event.find('location').next
-    date = event.find('start_date').next[:-5]
-    owner = event.find('contact_person').next
-    email = event.find('contact_email').next
-    tags = event.findAll('tag')
-    tagsL = list()
-    for tag in tags:
-        tagsL.append(tag.next)
-    description = event.find('description').next
-    description = re.sub('<[^>]+>', '', description)
-    print("Title: ", title)
-    print("id: ", eid)
-    print("Location: ", location)
-    print("Date: ", date)
-    print("Owner: ", owner)
-    print("Email: ", email)
-    print("Tags: ", tagsL)
-    print("Description: ", description)
-    description = description.replace("\u00a0", "").replace("\u201c", "").replace("\u2026", "").replace("\u201d", "").replace("\u2019", "'")
-    eventL.append(eventC(title,eid,location,date,description,owner,email,tagsL))
+        events = soup.findAll('event')
+        for event in events:
+            print("\n\n\n\n Event:")
+            title = event.find('title').next
+            eid = event.find('event_id').next
+            location = event.find('location').next
+            date = event.find('start_date').next[:-5]
+            owner = event.find('contact_person').next
+            email = event.find('contact_email').next
+            tags = event.findAll('tag')
+            tagsL = list()
+            for tag in tags:
+                tagsL.append(tag.next)
+            description = event.find('description').next
+            description = re.sub('<[^>]+>', '', description)
+            print("Title: ", title)
+            print("id: ", eid)
+            print("Location: ", location)
+            print("Date: ", date)
+            print("Owner: ", owner)
+            print("Email: ", email)
+            print("Tags: ", tagsL)
+            print("Description: ", description)
+            description = description.replace("\u00a0", "").replace("\u201c", "").replace("\u2026", "").replace("\u201d", "").replace("\u2019", "'")
+            eventL.append(eventC(title,eid,location,date,description,owner,email,tagsL))
 
 with open('events.json', 'w') as outfile:
     json.dump(eventL, outfile, default=lambda o: o.__dict__, indent=4)
