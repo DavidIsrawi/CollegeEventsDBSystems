c73f46cd6d6601203cbaa1ecc0d19c02f9bd8b0a
David <davidisrawi@gmail.com>
2017-03-28 00:33:45 -0400

    all events in db

--- scrapper/events.py
+++ scrapper/events.py
@@ -9,11 +9,12 @@ import re
 eventL = list()
 
 class eventC:
-    def __init__(self, title, eid, location, date, description, owner, email, tags):
+    def __init__(self, title, eid, location, date, dateEnd, description, owner, email, tags):
         self.title = title
         self.eid = eid
         self.location = location
         self.date = date
+        self.dateEnd = dateEnd
         self.description = description
         self.owner = owner
         self.email = email
@@ -35,6 +36,7 @@ for month in range (3,6):
             eid = event.find('event_id').next
             location = event.find('location').next
             date = event.find('start_date').next[:-5]
+            dateEnd = event.find('end_date').next[:-5]
             owner = event.find('contact_person').next
             email = event.find('contact_email').next
             tags = event.findAll('tag')
@@ -47,12 +49,13 @@ for month in range (3,6):
             print("id: ", eid)
             print("Location: ", location)
             print("Date: ", date)
+            print("Date End: ", dateEnd)
             print("Owner: ", owner)
             print("Email: ", email)
             print("Tags: ", tagsL)
             print("Description: ", description)
             description = description.replace("\u00a0", "").replace("\u201c", "").replace("\u2026", "").replace("\u201d", "").replace("\u2019", "'")
-            eventL.append(eventC(title,eid,location,date,description,owner,email,tagsL))
+            eventL.append(eventC(title,eid,location,date,dateEnd,description,owner,email,tagsL))
 
 with open('events.json', 'w') as outfile:
     json.dump(eventL, outfile, default=lambda o: o.__dict__, indent=4)
---
+++ scrapper/eventsDB.py
@@ -0,0 +1,68 @@
+import json
+import requests, time
+
+request = requests.Session()
+
+def makeTwo(number):
+    if len(str(number)) == 1:
+        return '0'+str(number)
+    else:
+        return number
+
+# JSON of RSOs
+with open('events.json') as data:
+    events = json.load(data)
+
+eventType = ['Social', 'Fundraising', 'Tech Talks', 'Sports', 'Health', 'SGA', 'Politics', 'Finances']
+month = {'Mar':'03', 'Apr':'04', 'May':'05'}
+counter = 0
+
+for event in events[1:]:
+
+    startD = event["date"].split(" ")
+    endD = event["dateEnd"].split(" ")
+
+    startDay = makeTwo(startD[1])
+    startMonth = month[startD[2]]
+    startYear = startD[3]
+    endDay = makeTwo(endD[1])
+    endMonth = month[endD[2]]
+    endYear = endD[3]
+
+    print("-- Event --")
+    print("Title: ", event["title"])
+    print("Description: ", event["description"])
+    print("Location: ", event["location"])
+    print("Tags: ", event["tags"])
+    print("eid: ", event["eid"])
+    print("Type: ", eventType[counter%len(eventType)])
+    print("Contact Name: ", event["owner"])
+    print("Contact Email: ", event["email"])
+    print("Start date: |", startYear, startMonth, startDay, "|")
+    print("End date: |", endYear, endMonth, endDay, "|")
+    print("Start time: |", event["date"][-9:-1], "|")
+    print("End time: |", event["dateEnd"][-9:-1], "|")
+    print()
+
+    payload = {
+        'name':event["title"].replace("'",""),
+        'description':event["description"].replace("'",""),
+        'location_name':event["location"].replace("'",""),
+        'tags':event["tags"],
+        'event_id':event["eid"],
+        'category':'Public',
+        'type':eventType[counter%len(eventType)],
+        'start_date':startYear+'-'+startMonth+'-'+startDay,
+        'end_date':endYear+'-'+endMonth+'-'+endDay,
+        'start_time':event["date"][-9:-1],
+        'end_time':event["dateEnd"][-9:-1],
+        'contact_name':event["owner"],
+        'contact_email':event["email"],
+        'status': "Approved"
+    }
+
+    response = request.post('http://teamflightclubproject.com/createEvent.php',data=payload)
+    print(response.content)
+
+    print()
+    counter += 1
