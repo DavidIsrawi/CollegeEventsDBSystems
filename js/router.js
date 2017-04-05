c31229273f00453354ac417b68dadd5ab0aee0db
David <davidisrawi@gmail.com>
2017-04-05 10:19:50 -0400

    trying routing for event

--- js/router.js
+++ js/router.js
@@ -1,8 +1,19 @@
 app.config(['$routeProvider', function($routeProvider) {
     console.log("router");
     $routeProvider
-    .when("/event/:id", {
-        controller : "customersCtrl",
+    .when("/event", {
+        controller : ['$routeParams', function($routeParams){
+            var self = this;
+            self.Event_id =  $routeParams.Event_id;
+            self.Name = $routeParams.Name;
+            self.Description = $routeParams.Description;
+            self.Contact_name = $routeParams.Contact_name;
+            self.Contact_email = $routeParams.Contact_email;
+            self.Start_date = $routeParams.Start_date;
+            self.End_date = $routeParams.End_date;
+            self.Location_name = $routeParams.Location_name;
+        }],
+        controllerAs: 'ctrl',
         templateUrl : "/event.html"
     })
     .when("/events", {
@@ -10,6 +21,7 @@ app.config(['$routeProvider', function($routeProvider) {
         templateUrl : "/events.html"
     })
     .otherwise({
-        redirecto: "/events"
+        controller : "customersCtrl",
+        templateUrl : "/events.html"
     });
 }]);
